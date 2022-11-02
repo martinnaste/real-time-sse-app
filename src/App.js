5705db0bcc624082039c9f7e597f8485a8d33179
martinnaste <martin.naste97@hotmail.com>
2022-11-02 15:26:32 +0800

    Fin

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ReactTable from "react-table";
+import "react-table/react-table.css";
+import { getInitialFlightData } from "./DataProvider";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor(props) {
+		super(props);
+			this.state = {
+			data: getInitialFlightData()
+		};
+
+		this.columns = [
+			{
+				Header: "Origin",
+				accessor: "origin"
+			},
+			{
+				Header: "Flight",
+				accessor: "flight"
+			},
+			{
+				Header: "Arrival",
+				accessor: "arrival"
+			},
+			{
+				Header: "State",
+				accessor: "state"
+			}
+		];
+
+		this.eventSource = new EventSource("http://localhost:5001/events");
+ 	}
+
+	stopUpdates() {
+		this.eventSource.close();
+	}
+
+	componentDidMount() {
+		this.eventSource.addEventListener("flightStateUpdate", e =>
+			this.updateFlightState(JSON.parse(e.data))
+		);
+		this.eventSource.addEventListener("flightRemoval", e =>
+			this.removeFlight(JSON.parse(e.data))
+		);
+		this.eventSource.addEventListener("closedConnection", e =>
+			this.stopUpdates()
+		);
+	}
+	
+	removeFlight(flightInfo) {
+		const newData = this.state.data.filter(
+			item => item.flight !== flightInfo.flight
+		);
+
+		this.setState(Object.assign({}, { data: newData }));
+	}
+	
+	updateFlightState(flightState) {
+		let newData = this.state.data.map(item => {
+			if (item.flight === flightState.flight) {
+			item.state = flightState.state;
+			}
+			return item;
+		});
+
+		this.setState(Object.assign({}, { data: newData }));
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<button onClick={() => this.stopUpdates()}>Stop updates</button>
+				<ReactTable
+					data={this.state.data}
+					columns={this.columns}
+				/>
+				<button onClick={() => this.makeChange()}>Make change</button>
+			</div>
+		);
+	  }
 }
 
 export default App;
